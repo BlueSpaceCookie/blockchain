96213aa1fd1276471baecd5e649877d3aeddae98
Monnet Joris <joris.monnet@he-arc.ch>
2021-04-29 16:16:27 +0200

    fix main

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ if __name__ == '__main__':
     firstChain = BlockChain()
     firstChain.createFirstBlock()
     print(firstChain)
-    firstWallet = wallet.create_wallet()
+    firstWallet = wallet.create_wallet() 
     secondWallet = wallet.create_wallet()
+    print("Second Wallet values = "+ str(secondWallet))
+
     firstChain.mine(firstWallet["address"])
     firstChain.mine(secondWallet["address"])
     firstChain.createTransaction(firstWallet["address"],secondWallet["address"],5)
